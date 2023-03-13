2aa1c5022c36f525c7bdb3774560df1ca3ed3158
101001000 <kike.delacalle.montilla@gmail.com>
2023-03-13 18:52:31 +0100

    Added compiler to extraction

--- src/Extraction.v
+++ src/Extraction.v
@@ -1,6 +1,14 @@
-From ASL Require import
-  Ast.
-Import Extraction.
+From ASL Require Import
+ Ast Compiler.
+From Coq Require Import
+ Extraction String.
+
+Local Open Scope string_scope.
+
 Require ExtrOcamlNatInt.
+Require ExtrOcamlString.
+
+Extraction Language OCaml.
 
 Extraction Library Ast.
+Extraction Library Compiler.
