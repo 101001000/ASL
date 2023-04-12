982af5eb04ff3ed1339eae063ada6cc31cfb8919
101001000 <kike.delacalle.montilla@gmail.com>
2023-04-12 11:45:07 +0200

    ast cleanup

--- src/ASL/AST.v
+++ src/ASL/AST.v
@@ -1,11 +1,7 @@
-From Coq Require Import
-  Strings.String.
+From Coq Require Import Strings.String.
 
 (* We need to import the Vellvm int32*)
-From Vellvm Require Import
-  Semantics.DynamicValues.
-
-Local Open Scope string_scope.
+From Vellvm Require Import Semantics.DynamicValues.
 
 Inductive expr : Type :=
 | Lit (_ : int32).
