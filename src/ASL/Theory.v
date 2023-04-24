527066c2a6ccbc3709e0a8ad1227b74a6b6f11a1
101001000 <kike.delacalle.montilla@gmail.com>
2023-04-24 15:15:01 +0200

    SetVar simpl lemma

--- src/ASL/Theory.v
+++ src/ASL/Theory.v
@@ -43,3 +43,34 @@ Proof.
   reflexivity.
 Qed.
 
+Lemma interp_asl_SetVar : forall x g v (E:Type->Type),
+    interp_asl (E:=E) (trigger (SetVar x v)) g ≈ Ret (FMapAList.alist_add x v g, tt).
+Proof.
+  intros.
+  unfold interp_asl. simpl.
+  rewrite unfold_interp. simpl. cbn. simpl.
+  unfold resum, ReSum_id, id_, Id_IFun. 
+  unfold cat, handle_State. cbn. unfold Cat_Handler. simpl. unfold Handler.cat. rewrite unfold_interp. simpl. unfold subevent. unfold resum, ReSum_id, id_, Id_IFun.
+  setoid_rewrite interp_ret.
+  unfold inl_.
+  unfold Inl_sum1_Handler. 
+  unfold Handler.inl_.
+  unfold Handler.htrigger.
+  rewrite bind_trigger.
+  unfold MapDefault.interp_map. simpl.
+  rewrite bind_vis_. simpl.
+  rewrite interp_state_vis . simpl.
+  unfold MapDefault.handle_map. simpl.
+  unfold case_. simpl.
+  unfold Case_sum1.
+  unfold case_sum1.
+  setoid_rewrite bind_tau_.
+  setoid_rewrite bind_ret_.
+  unfold fst. simpl.
+  setoid_rewrite bind_ret_.
+  setoid_rewrite interp_state_tau  .
+  setoid_rewrite interp_state_tau  .
+  setoid_rewrite interp_state_ret . simpl.
+  repeat rewrite tau_eutt.
+  reflexivity.
+Qed.
