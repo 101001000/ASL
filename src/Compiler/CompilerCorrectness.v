4e34d72c11838217c8cfab5f6fc7d3431b0381f1
101001000 <kike.delacalle.montilla@gmail.com>
2023-04-24 15:15:52 +0200

    Verified assignment

--- src/Compiler/CompilerCorrectness.v
+++ src/Compiler/CompilerCorrectness.v
@@ -6,6 +6,7 @@ From ITree       Require Import ITree ITreeFacts.
 From LLVMExtra   Require Import Utils.
 
 Import SemNotations.
+Import ITreeNotations.
 
 Open Scope string_scope.
 
@@ -14,14 +15,15 @@ let asl_env := fst r1 in
 let m_llvm := fst r2 in
 let l_llvm := fst (snd r2) in
 let g_llvm := fst (snd (snd r2)) in
-  Renv asl_env g_llvm l_llvm m_llvm.
+  Renvh asl_env g_llvm l_llvm m_llvm.
 
 Definition bisimilar (t1 : itree (State +' CallE +' PickE +' UBE +' DebugE +' FailureE) unit) (t2 : itree (instr_E) uvalue)  :=
   forall g_llvm l_llvm m_llvm g_asl,
-    Renv g_asl g_llvm l_llvm m_llvm ->
+    Renvh g_asl g_llvm l_llvm m_llvm ->
       eutt eutt_inv
        (interp_asl t1 g_asl)
        (ℑ3 t2 g_llvm l_llvm m_llvm).
+
 
 Theorem compiler_correct (s:stmt) :
   bisimilar (denote_asl s) (denote_cfg (compile s)).
@@ -36,19 +38,23 @@ Proof.
   induction s.
   
   + (* Assign *) 
-    unfold compile; simpl.
-    unfold gen_alloc_instr; simpl.
+    rewrite DenotationTheory.denote_code_app.
+    simpl.
     (* Because our expressions can only be Lit, destruct will only throw one case *)
     destruct e; simpl.
-    rewrite DenotationTheory.denote_code_cons.
-    rewrite bind_bind.
+    setoid_rewrite DenotationTheory.denote_code_singleton.
+    unfold gen_alloc_instr.    
     rewrite InterpreterCFG.interp_cfg3_bind.
-    rewrite simpl_alloc.
-    repeat rewrite bind_ret_.
-    simpl.
-    
+    rewrite simpl_alloca_assign.
+    repeat rewrite bind_ret_.  
+    rewrite InterpreterCFG.interp_cfg3_ret.
+    rewrite interp_asl_SetVar.
+    red.
+    apply eqit_Ret.
 
-    give_up.
+    unfold eutt_inv. simpl.
+    apply RenvhAssign.
+    auto.
   + (* Skip *)
     rewrite DenotationTheory.denote_code_nil.
     repeat rewrite bind_ret_.
@@ -57,4 +63,4 @@ Proof.
     red.
     apply eqit_Ret.
     auto.
-Admitted.
+Qed.
